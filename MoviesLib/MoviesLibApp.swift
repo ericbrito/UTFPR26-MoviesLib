4c4e677fdfbd2a719f24e3994c522ed97ef37d2f
Eric Brito <eric_brito@msn.com>
2026-06-27 08:26:59 -0300

    Initial Commit

---
+++ MoviesLib/MoviesLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesLibApp.swift
+//  MoviesLib
+//
+//  Created by Eric Alves Brito on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
